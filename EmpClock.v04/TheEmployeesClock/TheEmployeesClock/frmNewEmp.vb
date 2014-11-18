9f64a2e0a8a13f7cd2e7ed8f436f7e05f1937e03
crazycoolcat05 <crazycoolcat05@gmail.com>
2014-11-18 15:40:21 -0800

    Placed comment on frmNewEmp

--- EmpClock.v04/TheEmployeesClock/TheEmployeesClock/frmNewEmp.vb
+++ EmpClock.v04/TheEmployeesClock/TheEmployeesClock/frmNewEmp.vb
@@ -3,6 +3,6 @@
     Private Sub frmNewEmp_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the '_TheEmployeesClock_dBDataSet.Employees' table. You can move, or remove it, as needed.
         Me.EmployeesTableAdapter.Fill(Me._TheEmployeesClock_dBDataSet.Employees)
-
+        ' test github
     End Sub
 End Class
